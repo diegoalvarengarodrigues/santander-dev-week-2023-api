61090a022be6b35afbf1970e615935fd709700f2
diego <diegoalvarengarodrigues@gmail.com>
2023-10-20 16:47:52 -0300

    controller

---
+++ src/main/java/me/dio/santanderdevweek2023api/controller/UserController.java
@@ -0,0 +1,35 @@
+package me.dio.santanderdevweek2023api.controller;
+
+import me.dio.santanderdevweek2023api.model.User;
+import me.dio.santanderdevweek2023api.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+}
