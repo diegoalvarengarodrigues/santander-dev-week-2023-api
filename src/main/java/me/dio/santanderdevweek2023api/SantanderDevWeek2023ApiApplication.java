f0ff79de280fc7a0329b669b1f1880d819964ad4
diego <diegoalvarengarodrigues@gmail.com>
2023-10-20 16:13:26 -0300

    first commit

---
+++ src/main/java/me/dio/santanderdevweek2023api/SantanderDevWeek2023ApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweek2023api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2023ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2023ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santanderdevweek2023api/SantanderDevWeek2023ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweek2023api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
